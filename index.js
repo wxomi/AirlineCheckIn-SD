abf0898e19bb9c7267a15bcc75822d3f902f9ed0
wxomi <samsepi0I@outlook.com>
2023-06-15 23:26:29 +0530

    README.md

--- index.js
+++ index.js
@@ -72,49 +72,6 @@ const bookSeat = (userId, tripId) => {
   });
 };
 
-// const bookSeat = (userId, tripId) => {
-//   return new Promise((resolve, reject) => {
-//     pool.getConnection((error, connection) => {
-//       if (error) {
-//         reject(error);
-//         return;
-//       }
-//       const selectQuery =
-//         "SELECT id, name, trip_id, user_id FROM Seats WHERE trip_id = ? AND user_id IS NULL LIMIT 1";
-//       connection.query(selectQuery, [tripId], (error, results) => {
-//         if (error) {
-//           connection.release(); // Release the connection back to the pool
-//           reject(error);
-//           return;
-//         }
-
-//         if (results.length === 0) {
-//           connection.release(); // Release the connection back to the pool
-//           resolve("No available seats.");
-//           return;
-//         }
-
-//         console.log(results, userId);
-//         const updateQuery = "UPDATE Seats SET user_id = ? WHERE id = ?";
-//         connection.query(
-//           updateQuery,
-//           [userId, results[0].id], // Use the seat ID from the query results
-//           (error, updateResults) => {
-//             connection.release(); // Release the connection back to the pool
-//             if (error) {
-//               reject(error);
-//             } else {
-//               resolve(
-//                 `User ${userId} successfully booked a seat for trip ${tripId}`
-//               );
-//             }
-//           }
-//         );
-//       });
-//     });
-//   });
-// };
-
 const numThreads = 120;
 const bookingPromises = [];
 for (let i = 1; i <= numThreads; i++) {
